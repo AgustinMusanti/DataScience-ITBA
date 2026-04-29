89fd2473ab105ebaa02deeff7858e2c78254813f
Agustin Musanti <138232526+AgustinMusanti@users.noreply.github.com>
2026-04-29 20:18:05 -0300

    >>

--- DataWarehouse & OLAP/TP1/Solución.sql
+++ DataWarehouse & OLAP/TP1/Solución.sql
@@ -1,5 +1,4 @@
 /* 1.1
-
 La tabla measurement tiene 53 mediciones. Tiene el  ítem_code pero no el nombre del 
 contaminante. Para poder listar el nombre de dicho contaminante se precisa usar la información de 
 2 tablas: measurement y item.  
@@ -7,3 +6,13 @@ contaminante. Para poder listar el nombre de dicho contaminante se precisa usar 
 más con el nombre del contaminante? ¿Por qué se obtienen  424  ( 53 * 8)  tuplas? 
 Explicar */
 SELECT  measurement.*, name
+FROM measurement,  item 
+
+/* RTA: por producto cartesiano.
+Cuando se escribe FROM measurement, item sin una condición de JOIN, SQL combina cada fila de measurement con cada fila de item. */
+
+Solución:
+
+SELECT measurement.*, item.name
+FROM measurement
+JOIN item ON measurement.item_code = item.code;
